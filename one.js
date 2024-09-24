9bbbbd8e5a579310021e3dd0382bf2cad45812b0
abhaydeep24 <singhabhaydeep26@gmail.com>
2024-09-25 00:44:26 +0530

    initial commit

---
+++ one.js
@@ -0,0 +1,22 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-content");
+
+function opentab(tabname){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.left="0";
+}
+function closemenu(){
+    sidemenu.style.left="-230px";
+}
